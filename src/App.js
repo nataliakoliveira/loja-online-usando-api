fa3bd441dd272e1a8d34c955e15ecf1526f2730e
nataliakoliveira <nataliaakellyy@gmail.com>
2022-09-08 18:49:16 -0300

    requisito3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './Components/Home';
+import Carrinho from './Components/Carrinho';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route path="/" component={ Home } />
+          <Route exact path="/" component={ Home } />
+          <Route path="/carrinho" component={ Carrinho } />
         </Switch>
       </BrowserRouter>
     );
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   render() {
     return (
-      <h2 data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </h2>
+      <div>
+        <h2 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h2>
+        <Link data-testid="shopping-cart-button" to="/carrinho">Carrinho</Link>
+
+      </div>
     );
   }
 }
